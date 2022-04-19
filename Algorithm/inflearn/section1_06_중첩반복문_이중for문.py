46d8c3ca77a510a2bc8bb876c47cd62aab8cebf3
greencoffee1635 <166354@naver.com>
2022-04-19 16:37:32 +0900

    Rename basic to section1

--- Algorithm/inflearn/section1_06_중첩반복문_이중for문.py
+++ Algorithm/inflearn/section1_06_중첩반복문_이중for문.py
@@ -3,7 +3,7 @@
 for i in range(5):
     for j in range(5):
         print(j, end=' ')
-    print() # 줄바꿈
+    print()  # 줄바꿈
 
 '''
 0 1 2 3 4
@@ -16,8 +16,8 @@ for i in range(5):
 for i in range(5):
     print('i:', i, sep='', end=' - ')
     for j in range(5):
-        print('j:', j, sep='', end=' ') # 줄바꿈 안함
-    print() # 줄바꿈
+        print('j:', j, sep='', end=' ')  # 줄바꿈 안함
+    print()  # 줄바꿈
 
 '''
 i:0 - j:0 j:1 j:2 j:3 j:4
@@ -29,7 +29,7 @@ i:4 - j:0 j:1 j:2 j:3 j:4
 
 for i in range(5):
     for j in range(5):
-        print('*', end= ' ') # j가 5바퀴 도니까 *을 5개 찍는다.
+        print('*', end=' ')  # j가 5바퀴 도니까 *을 5개 찍는다.
     print()
 
 '''
@@ -41,7 +41,7 @@ for i in range(5):
 '''
 
 for i in range(5):
-    for j in range(i + 1): # 처음에 i는 0이기 때문에 +1을 한다.
+    for j in range(i + 1):  # 처음에 i는 0이기 때문에 +1을 한다.
         print('*', end=' ')
     print()
 
