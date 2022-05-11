eeda27f3c1d53e9076c15e24f23b372f93871cd0
greencoffee1635 <166354@naver.com>
2022-05-11 20:38:20 +0900

    Fix minor typo

--- Algorithm/inflearn/section2_08_뒤집은소수.py
+++ Algorithm/inflearn/section2_08_뒤집은소수.py
@@ -26,15 +26,15 @@ def reverse(x):
     res = 0
     while x > 0:
         t = x % 10
-        res = res*10+t
-        x = x//10
+        res = res * 10 + t
+        x = x // 10
     return res
 
 
 def isPrime(x):  # 소수인지 확인해주는 함수
     if x == 1:
         return False  # 1이면 바로 False
-    for i in range(2, x//2+1):
+    for i in range(2, x // 2 + 1):
         if x % i == 0:
             return False  # 약수가 발견되면 False 종료
     else:
