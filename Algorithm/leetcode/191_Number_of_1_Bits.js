a68f5f76e51b706e3e56de2bb5f2606292c8ea76
jk <jkwak1635@gmail.com>
2024-01-01 18:36:30 +0900

    Add Number of 1 Bits Solution

---
+++ Algorithm/leetcode/191_Number_of_1_Bits.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+// Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+// The input must be a binary string of length 32.
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+
+let n = 0b00000000000000000000000000001011 // 3
+// let n = 0b11111111111111111111111111111101 // 31
+
+const hammingWeight = function (n) {
+    let count = 0;
+    for (let i = 0; i < 32; i++) {
+        if ((n & 1) === 1) {
+            count++;
+        }
+        n = n >>> 1;
+    }
+    return count;
+};
+
+console.log(hammingWeight(n))
