84622aed052ea89f5b462c64268a44822ab73fd1
jk <jkwak1635@gmail.com>
2024-02-10 17:26:58 +0900

    Fix formatting

--- Algorithm/programmers/Lv1_나누어떨어지는숫자배열.js
+++ Algorithm/programmers/Lv1_나누어떨어지는숫자배열.js
@@ -1,27 +1,27 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=javascript
+// https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=javascript
 
 // array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
 // divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
 
 const arr = [5, 9, 7, 10];
-const divisor = 5 // [5, 10]
+const divisor = 5; // [5, 10]
 // const arr = [2, 36, 1, 3];
 // const divisor = 1; // [1, 2, 3, 36]
 // const arr = [3, 2, 6];
-// const divisor = 10; // [-1]  
+// const divisor = 10; // [-1]
 
 function solution(arr, divisor) {
-    let answer = [];
+    const answer = [];
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] % divisor === 0) {
+        if (arr[i] % divisor === 0) {
             answer.push(arr[i]);
         }
     }
-    
+
     if (answer.length === 0) {
         return [-1];
     }
-    
+
     return answer.sort((a, b) => a - b);
 }
 
